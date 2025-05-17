9193e6f8045bf5edebc68f259d0620bcf4284439
dariolpz05 <aodariolopez@gmail.com>
2025-05-17 16:27:34 +0200

    Modificando start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Valor de DATABASE_URL: $DATABASE_URL"
+export DATABASE_URL="$DATABASE_URL"
 
 echo "Esperando base de datos..."
 until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do

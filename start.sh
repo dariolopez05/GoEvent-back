2ee2b1fcc679ffa677de5b7f842036f6320f86f6
dariolpz05 <aodariolopez@gmail.com>
2025-05-17 16:41:37 +0200

    Modificando start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-echo "Probando conexión a la base de datos..."
-php test-db.php
+#!/bin/bash
+set -e
+
+echo "Valor de DATABASE_URL: $DATABASE_URL"
+
+echo "Esperando base de datos..."
+
+until php test-db.php | grep -q "OK"; do
+  echo "Base de datos no disponible, esperando 5 segundos..."
+  sleep 5
+done
+
+echo "Ejecutando migraciones..."
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+
+echo "Iniciando servidor..."
+php -S 0.0.0.0:${PORT} -t public
